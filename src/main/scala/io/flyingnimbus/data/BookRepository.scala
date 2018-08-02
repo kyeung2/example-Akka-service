95b6fd78515dbf15f5df830f313a35a36802d861
Kye Yeung <yeung.kye@gmail.com>
2018-08-02 14:22:47 +0100

    tweaking

--- src/main/scala/io/flyingnimbus/data/BookRepository.scala
+++ src/main/scala/io/flyingnimbus/data/BookRepository.scala
@@ -3,15 +3,15 @@ package io.flyingnimbus.data
 import io.flyingnimbus.domain.Book
 import org.mongodb.scala._
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
   * @author Kye
   */
-class BookRepository(collection: MongoCollection[Book]) {
+class BookRepository(collection: MongoCollection[Book])(implicit executionContext: ExecutionContext) {
 
   def findAll(): Future[Seq[Book]] = collection.find().collect().head()
 
-  def save(user: Book): Unit = collection.insertOne(user)
+  def save(user: Book): Future[String] = collection.insertOne(user).head().map { _ => user._id.toHexString }
 
 }
